050618940a345b46dced23407b9e48bf78eefa87
Edgars Vanags <edgars.vanags1@gmail.com>
2020-11-03 14:49:01 +0200

    Friend Tracker Repository: Implemented FriendTrackerRepositoryImpl (#2)
    * Implemented FriendTrackingRepository

---
+++ FriendFinder/Data/Repositories/Entities/LocationUpdate.swift
@@ -0,0 +1,14 @@
+//
+//  MacOS 10.15
+//  Swift 5.0
+//  FriendFinder
+//  @esesmuedgars
+//
+
+import Foundation
+import CoreLocation
+
+struct LocationUpdate: Identifiable {
+    let id: Int
+    let location: CLLocation
+}
